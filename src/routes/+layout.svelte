19a18bfedd1ce966ad9740ff05919f3d69f74c8f
bereri01 <bereri01@students.zhaw.ch>
2026-05-02 15:57:37 +0200

    nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,33 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+  import "./app.css";
+  let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">MacroMate</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/dailtracking">Daily Tracking</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/caloriecalculator">Calorie Calculator</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/food">Food Macros</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/recipes">Recipes</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-{@render children()}
+<div class="container mt-3">
+  {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,2 @@
-<script>
-  import "./app.css";
-  let { children } = $props();
-</script>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">MacroMate</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/dailtracking">Daily Tracking</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<div class="container mt-3">
-  {@render children()}
-</div>
+<h1>Welcome to ScreenStack</h1>
+<p>Your tracker and search engine for movies and tv shows</p>
