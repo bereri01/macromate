7876cd0b35f7ee1abe81ae25ca58854fd52fd699
bereri01 <bereri01@students.zhaw.ch>
2026-05-02 16:05:12 +0200

    nav bar correction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,25 +5,35 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
   <div class="container-fluid">
+    <!-- Left: /Home -->
     <a class="navbar-brand" href="/">MacroMate</a>
+
+    <!-- Toggler for mobile -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+
+      <!-- Center/Right: Nav links pushed to the right with ms-auto -->
+      <ul class="navbar-nav ms-auto me-3">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/dailtracking">Daily Tracking</a>
+          <a class="nav-link" href="/dailytracking">Daily Tracking</a>
         </li>
-		<li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/caloriecalculator">Calorie Calculator</a>
+        <li class="nav-item">
+          <a class="nav-link" href="/caloriecalculator">Calorie Calculator</a>
         </li>
-		<li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/food">Food Macros</a>
+        <li class="nav-item">
+          <a class="nav-link" href="/food">Food Macros</a>
         </li>
-		<li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/recipes">Recipes</a>
+        <li class="nav-item">
+          <a class="nav-link" href="/recipes">Recipes</a>
         </li>
       </ul>
+
+      <!-- Far Right: Login Button -->
+      <a class="btn btn-outline-light" href="/login">Login</a>
+
     </div>
   </div>
 </nav>
